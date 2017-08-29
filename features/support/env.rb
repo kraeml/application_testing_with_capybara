7a4a73e7ea2bdd18825905cca7ca3b0006dcfb3c
kraeml <kraeml@sesamestreet.franken.de>
2017-08-29 10:40:12 +0200

    Adding poltergeist in env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,12 +2,37 @@ require 'bundler/setup'
 require 'capybara/cucumber'
 require 'rspec/expectations'
 require_relative '../../sinatra/app'
+require 'capybara/cucumber'
+require 'capybara/poltergeist'
 
-Capybara.default_driver = :selenium
+#Capybara.default_driver = :selenium
 #Capybara.default_driver = :rack_test #Use this when looking at the examples in chapter 3 and compare it with using Selenium!
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
-end
+#Capybara.register_driver :selenium do |app|
+#  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+#end
 
-Capybara.app = BookReview #our Sinatra app
+#Capybara.app = BookReview #our Sinatra app
+
+if ENV['IN_BROWSER']
+  # On demand: non-headless tests via Selenium/WebDriver
+  # To run the scenarios in browser (default: Firefox), use the following command line:
+  # IN_BROWSER=true bundle exec cucumber
+  # or (to have a pause of 1 second between each step):
+  # IN_BROWSER=true PAUSE=1 bundle exec cucumber
+  Capybara.default_driver = :selenium
+  AfterStep do
+    sleep (ENV['PAUSE'] || 0).to_i
+  end
+else
+  # DEFAULT: headless tests with poltergeist/PhantomJS
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(
+      app,
+      window_size: [1280, 1024]#,
+      #debug:       true
+    )
+  end
+  Capybara.default_driver    = :poltergeist
+  Capybara.javascript_driver = :poltergeist
+end
